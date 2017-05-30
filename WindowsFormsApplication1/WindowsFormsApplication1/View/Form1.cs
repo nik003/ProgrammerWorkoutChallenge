cae76d326e8d2c7c09d0951a44b03546811e4f1d
nik003 <nikko.dahlberg@hotmail.com>
2017-05-30 15:30:14 +0200

    base files for project

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/View/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApplication1.View;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+
+        }
+        private TextBox AddTextboxes(int nrOfExercises) 
+        {
+            TextBoxes tbs = new TextBoxes();
+            List<TextBox> boxes = tbs.createTextBoxes(nrOfExercises);
+            for (int i = 0; i < boxes.Count; i++)
+            {
+                this.Controls.Add(boxes[i]);
+            }
+            
+          
+
+            return tb;
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/View/TextBoxes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1.View
+{
+    class TextBoxes
+    {
+        private List<TextBox> textBoxes;
+        public TextBoxes()
+        {
+
+        }
+        public List<TextBox> createTextBoxes(int excercises)
+        {      
+            for(int i = 0; i < excercises; i++)
+            {
+
+            }
+            return textBoxes;
+        }
+    }
+}
